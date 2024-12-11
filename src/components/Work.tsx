ddefe4214aa6718c0fe3cd9d2d7856fdcdf5477b
Shaban-Satti <shabansatti165@gmail.com>
2024-12-11 17:27:43 +0500

    text align center

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -10,7 +10,7 @@ export function Work() {
   return (
     <section id="work" className="py-12">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <h2 className="text-4xl font-bold text-white mb-12 flex justify-center "  
+        <h2 className="text-4xl font-bold text-white mb-12 flex justify-center text-center"  
    style={{
     background: 'linear-gradient(90deg, #ff8660, #d5491d)',
     WebkitBackgroundClip: 'text',
